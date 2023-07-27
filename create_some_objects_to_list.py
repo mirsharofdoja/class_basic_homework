6a9b3224eb67191fd9693e13a85068d95c49af00
mirsharofdoja <mirsharoftursunaliyev@gmail.com>
2023-07-27 09:35:30 +0500

    done

--- create_some_objects_to_list.py
+++ create_some_objects_to_list.py
@@ -3,5 +3,9 @@ from create_one_attribute import Person
 #Create an object named "p1" whose name is "Anvar"
 #Create an object named "p2" whose name is "Shavkat"
 #Create an object named "p3" whose name is "Jasur"
+p1=Person('Anvar')
+p2=Person('Shavkat')
+p3=Person('Jasur')
 
 #Add these objects to the "persons" named list
+persons=[p1,p2,p3]
--- create_two_attribute.py
+++ create_two_attribute.py
@@ -1,3 +1,7 @@
 #Create a "Person" class
+class Person:
 
 #that has a name("name") and a age("age")
+    def __init__(self,name:str,age:int):
+        self.name=name
+        self.age=age
